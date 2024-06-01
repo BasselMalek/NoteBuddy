e661ff57bc719fea42fe548180df6fd8b5a899c6
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-06-01 16:29:32 +0300

    feat: add EAS buildflow and tuner proto

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout() {
                             color="black"
                         />
                     ),
-                    headerStyle: { backgroundColor: "#000" },
                 }}
             />
             <Tabs.Screen
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,47 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+    View,
+    ScrollView,
+    Text,
+    Button,
+    StyleSheet,
+    Platform,
+} from "react-native";
+import { PitchDetector } from "react-native-pitch-detector";
+import { PERMISSIONS, check, RESULTS, request } from "react-native-permissions";
 
 export default function index() {
+    const [sound, setSound] = useState();
+    const [pitch, setPitch] = useState<string>();
+    const [test, setTest] = useState();
+
+    async function startTuner() {
+        let status = await check(PERMISSIONS.ANDROID.RECORD_AUDIO);
+        if (status != RESULTS.GRANTED) {
+            const permReq = await request(PERMISSIONS.ANDROID.RECORD_AUDIO);
+            status = permReq;
+        }
+        if (
+            status === RESULTS.GRANTED &&
+            !(await PitchDetector.isRecording()).valueOf()
+        ) {
+            PitchDetector.start();
+            PitchDetector.addListener(console.log);
+        }
+    }
+    async function stopTuner() {
+        PitchDetector.stop();
+    }
+
     return (
-        <View>
-            <Text>HI!</Text>
-        </View>
+        <ScrollView>
+            <Text>
+                Hi! Press on the record/playback button to start playing audio
+                recorded after delay
+            </Text>
+            <Text>Current Pitch: {test}</Text>
+            <Button title="Start record/playback" onPress={startTuner} />
+            <Button title="Stop" onPress={stopTuner} />
+        </ScrollView>
     );
 }
