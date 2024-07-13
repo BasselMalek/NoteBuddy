9685495d8848f4f8d4fe13964bacb18326a509e8
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-07-13 03:21:01 +0300

    chore(entry): refactor internal state.

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -11,146 +11,212 @@ import { getAdaptaiveTheme } from "@/constants/Colors";
 import { useState, useEffect } from "react";
 import RatingSelector from "@/components/RatingSelector";
 
-interface Entrystruct {
+interface EntryData {
     date: Date;
-    title: string;
-    durationTime: string;
-    durationFrom: Date;
-    durationTo: Date;
-    rating: number;
-    desc: string;
+    title?: string;
+    durationTime?: string;
+    durationFrom?: Date;
+    durationTo?: Date;
+    rating?: number;
+    desc?: string;
+    toAdd: boolean;
+    toEdit?: boolean;
 }
-
-type EntryData = Entrystruct | null;
 
 function Entry(props: {
     isExpanded: boolean;
     style?: StyleProp<View>;
     entryData: EntryData;
+    onEntryChangeHandler: Function;
 }) {
-    const [editingActive, setEditingActive] = useState(false);
+    const [editingActive, setEditingActive] = useState(props.entryData.toAdd);
+    const [editingExistent, setEditingExistent] = useState(
+        !(props.entryData.title === undefined)
+    );
 
-    const [entryTitle, setEntryTitle] = useState("Today's Entry");
-    const [entryRating, setEntryRating] = useState(2);
-    const [entryDesc, setEntryDesc] = useState("");
+    const [activeEntryData, setActiveEntryData] = useState({
+        title: props.entryData?.title,
+        rating: props.entryData?.rating,
+        desc: props.entryData?.desc,
+        durationFrom: props.entryData.durationFrom,
+        durationTo: props.entryData.durationTo,
+        durationTime: props.entryData.durationTime,
+    });
 
-    const [durationFrom, setDurationFrom] = useState(new Date());
-    const [durationTo, setDurationTo] = useState(new Date());
-    const [entryDuration, setEntryDuration] = useState("0hrs 0m");
+    const setEntryTitle = (title: string) => {
+        setActiveEntryData((prevState) => ({
+            ...prevState,
+            title,
+        }));
+    };
+
+    const setEntryRating = (rating: number) => {
+        setActiveEntryData((prevState) => ({
+            ...prevState,
+            rating,
+        }));
+    };
+
+    const setEntryDesc = (desc: string) => {
+        setActiveEntryData((prevState) => ({
+            ...prevState,
+            desc,
+        }));
+    };
+
+    const setDurationFrom = (durationFrom: Date) => {
+        setActiveEntryData((prevState) => ({
+            ...prevState,
+            durationFrom,
+        }));
+    };
+
+    const setDurationTo = (durationTo: Date) => {
+        setActiveEntryData((prevState) => ({
+            ...prevState,
+            durationTo,
+        }));
+    };
+
+    const setEntryDuration = (durationTime: string) => {
+        setActiveEntryData((prevState) => ({
+            ...prevState,
+            durationTime,
+        }));
+    };
 
     useEffect(() => {
-        const m = (durationTo.valueOf() - durationFrom.valueOf()) / 1000 / 60;
+        const m =
+            (activeEntryData.durationTo === undefined
+                ? new Date().valueOf()
+                : activeEntryData.durationTo.valueOf() -
+                  (activeEntryData.durationFrom === undefined
+                      ? new Date().valueOf()
+                      : activeEntryData.durationFrom.valueOf())) /
+            1000 /
+            60;
         const hr = Math.floor(m / 60);
         setEntryDuration(hr + "hrs " + (m - hr * 60) + "m");
         return () => {};
-    }, [durationFrom, durationTo]);
+    }, [activeEntryData.durationFrom, activeEntryData.durationTo]);
 
-    if (props.entryData != null) {
-        return (
-            <PaperProvider theme={getAdaptaiveTheme()}>
-                <Card style={styles.expandedCard}>
-                    {(editingActive && (
+    return (
+        <PaperProvider theme={getAdaptaiveTheme()}>
+            <Card style={styles.expandedCard}>
+                {(editingActive && (
+                    <>
+                        <TextInput
+                            mode="outlined"
+                            value={activeEntryData.title}
+                            onChangeText={(text) => {
+                                setEntryTitle(text);
+                            }}
+                            label={"Title"}
+                        ></TextInput>
+                        <RatingSelector
+                            ratingState={
+                                activeEntryData.rating === undefined
+                                    ? 0
+                                    : activeEntryData.rating
+                            }
+                            ratingHandler={(entryRating: number) => {
+                                setEntryRating(entryRating);
+                            }}
+                            starColor={getAdaptaiveTheme().colors.secondary}
+                        ></RatingSelector>
+                        <DurationPicker
+                            fromValue={
+                                activeEntryData.durationFrom === undefined
+                                    ? new Date()
+                                    : activeEntryData.durationFrom
+                            }
+                            fromHandler={(start: any) => {
+                                setDurationFrom(start);
+                            }}
+                            toValue={
+                                activeEntryData.durationTo === undefined
+                                    ? new Date()
+                                    : activeEntryData.durationTo
+                            }
+                            toHandler={(end: any) => {
+                                setDurationTo(end);
+                            }}
+                        ></DurationPicker>
+                        <TextInput
+                            mode="outlined"
+                            value={activeEntryData.desc}
+                            label={"Entry"}
+                            onChangeText={(text) => {
+                                setEntryDesc(text);
+                            }}
+                            numberOfLines={100}
+                            multiline={true}
+                            contentStyle={{ height: 400 }}
+                        ></TextInput>
+                        <FAB
+                            icon={"check"}
+                            onPress={() => {
+                                if (editingExistent) {
+                                    props.onEntryChangeHandler({
+                                        ...activeEntryData,
+                                        toAdd: false,
+                                        toEdit: true,
+                                    });
+                                } else {
+                                    props.onEntryChangeHandler({
+                                        ...activeEntryData,
+                                        toAdd: true,
+                                    });
+                                }
+                                setEditingActive(false);
+                            }}
+                        ></FAB>
+                    </>
+                )) ||
+                    (!editingActive && (
                         <>
-                            <TextInput
-                                mode="outlined"
-                                value={entryTitle}
-                                onChangeText={(text) => {
-                                    setEntryTitle(text);
+                            <PaperText
+                                variant="titleLarge"
+                                style={{
+                                    fontWeight: "bold",
                                 }}
-                                label={"Title"}
-                            ></TextInput>
-                            <RatingSelector
-                                ratingState={entryRating}
-                                ratingHandler={(entryRating: number) => {
-                                    setEntryRating(entryRating);
+                            >
+                                {activeEntryData.title}
+                            </PaperText>
+                            <PaperText
+                                style={{
+                                    fontWeight: "thin",
+                                    fontStyle: "italic",
+                                    fontSize: 16,
                                 }}
-                                starColor={getAdaptaiveTheme().colors.secondary}
-                            ></RatingSelector>
-                            <DurationPicker
-                                fromValue={durationFrom}
-                                fromHandler={(start: any) => {
-                                    setDurationFrom(start);
+                            >
+                                {Array.from(
+                                    {
+                                        length:
+                                            activeEntryData.rating === undefined
+                                                ? 0
+                                                : activeEntryData.rating,
+                                    },
+                                    (i, k) => k
+                                ).map((i, k) => "★")}
+                            </PaperText>
+                            <PaperText
+                                variant="titleSmall"
+                                style={{
+                                    fontWeight: "thin",
+                                    fontStyle: "italic",
                                 }}
-                                toValue={durationTo}
-                                toHandler={(end: any) => {
-                                    setDurationTo(end);
-                                }}
-                            ></DurationPicker>
-                            <TextInput
-                                mode="outlined"
-                                value={entryDesc}
-                                label={"Entry"}
-                                onChangeText={(text) => {
-                                    setEntryDesc(text);
-                                }}
-                                numberOfLines={100}
-                                multiline={true}
-                                contentStyle={{ height: 400 }}
-                            ></TextInput>
+                            >
+                                {activeEntryData.durationTime}
+                            </PaperText>
+                            <PaperText style={{ paddingVertical: 30 }}>
+                                {activeEntryData.desc}
+                            </PaperText>
                         </>
-                    )) ||
-                        (!editingActive && (
-                            <>
-                                <PaperText
-                                    variant="titleLarge"
-                                    style={{
-                                        fontWeight: "bold",
-                                    }}
-                                >
-                                    {entryTitle}
-                                </PaperText>
-                                <PaperText
-                                    style={{
-                                        fontWeight: "thin",
-                                        fontStyle: "italic",
-                                        fontSize: 16,
-                                    }}
-                                >
-                                    {Array.from(
-                                        { length: entryRating },
-                                        (i, k) => k
-                                    ).map((i, k) => "★")}
-                                </PaperText>
-                                <PaperText
-                                    variant="titleSmall"
-                                    style={{
-                                        fontWeight: "thin",
-                                        fontStyle: "italic",
-                                    }}
-                                >
-                                    {entryDuration}
-                                </PaperText>
-                                <PaperText style={{ paddingVertical: 30 }}>
-                                    {entryDesc}
-                                </PaperText>
-                            </>
-                        ))}
-                    <FAB
-                        icon={editingActive ? "check" : "pencil"}
-                        style={styles.fab}
-                        onPress={() => {
-                            setEditingActive(!editingActive);
-                        }}
-                    ></FAB>
-                </Card>
-            </PaperProvider>
-        );
-    } else {
-        return (
-            <PaperProvider theme={getAdaptaiveTheme()}>
-                <Card style={styles.expandedCard}>
-                    <PaperText>No entry for today.</PaperText>
-                    <FAB
-                        icon={"plus"}
-                        style={styles.fab}
-                        onPress={() => {
-                            setEditingActive(true);
-                        }}
-                    ></FAB>
-                </Card>
-            </PaperProvider>
-        );
-    }
+                    ))}
+            </Card>
+        </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
