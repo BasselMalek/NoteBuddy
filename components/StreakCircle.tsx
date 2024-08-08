c309dcf31000ef84ee747f61fe6f5cf79b55da40
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2024-08-09 00:07:49 +0300

    styling: change bonus text.

--- components/StreakCircle.tsx
+++ components/StreakCircle.tsx
@@ -80,7 +80,7 @@ export default function StreakCircle(props: {
                             {`${bonus}x`}{" "}
                         </PaperText>
                         <PaperText style={{ fontSize: 12 }}>
-                            Pts bonus
+                            {"Point Gain"}
                         </PaperText>
                     </>
                 )}
